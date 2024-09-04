9152f6f8739b02393bc4989575de6affe173a1e2
Gabriel Calixto <gfcdev@outlook.com.br>
2024-09-04 20:49:24 -0300

    Update

---
+++ src/main/java/br/com/gfctech/starter/dto/PostDTO.java
@@ -0,0 +1,35 @@
+package br.com.gfctech.starter.dto;
+
+public class PostDTO {
+    private Long id;
+    private String content;
+    private long authorId;
+    private int likesCount;
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getContent() {
+        return content;
+    }
+    public void setContent(String content) {
+        this.content = content;
+    }
+    public long getAuthorId() {
+        return authorId;
+    }
+    public void setAuthorId(long authorId) {
+        this.authorId = authorId;
+    }
+    public int getLikesCount() {
+        return likesCount;
+    }
+    public void setLikesCount(int likesCount) {
+        this.likesCount = likesCount;
+    }
+
+
+    
+}
