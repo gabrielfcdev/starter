cf3300e8b86bede5ac3852081773b54a6625dbd5
Gabriel Calixto <gfcdev@outlook.com.br>
2024-09-05 21:38:27 -0300

    update

--- src/main/java/br/com/gfctech/starter/entity/UserEntity.java
+++ src/main/java/br/com/gfctech/starter/entity/UserEntity.java
@@ -1,7 +1,9 @@
 package br.com.gfctech.starter.entity;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -14,90 +16,20 @@ public class UserEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
-    private String usernames;
+
+    @Column(name = "username", nullable = false, unique=true)
+    private String username;
+
+    @Column()
     private String password;
     private String email;
-    private String fullName;
+    private String bio;
+    private List<Post> posts = new ArrayList<>();
+
+    private List<Group> groups = new ArrayList<>();
 
     @OneToMany(mappedBy = "author")
     private  List<PostEntity> posts;
 
-
-    @OneToMany(mappedBy = "user")
-    private List<CommentEntity> comments;
-
-    @OneToMany(mappedBy = "user")
-    private List<LikeEntity> likes;
-    
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsernames() {
-        return usernames;
-    }
-
-    public void setUsernames(String usernames) {
-        this.usernames = usernames;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getFullName() {
-        return fullName;
-    }
-
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
-
-    public List<PostEntity> getPosts() {
-        return posts;
-    }
-
-    public void setPosts(List<PostEntity> posts) {
-        this.posts = posts;
-    }
-
-    public List<CommentEntity> getComments() {
-        return comments;
-    }
-
-    public void setComments(List<CommentEntity> comments) {
-        this.comments = comments;
-    }
-
-    public List<LikeEntity> getLikes() {
-        return likes;
-    }
-
-    public void setLikes(List<LikeEntity> likes) {
-        this.likes = likes;
-    }
-
-    
-    
-
-    
 
 }
