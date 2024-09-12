95d3764d9c635c9c21e6e34923fb7da9a52800d6
Gabriel Calixto <gfcdev@outlook.com.br>
2024-09-12 20:40:36 -0300

    UserEntity implements

--- src/main/java/br/com/gfctech/starter/entity/UserEntity.java
+++ src/main/java/br/com/gfctech/starter/entity/UserEntity.java
@@ -1,6 +1,8 @@
 package br.com.gfctech.starter.entity;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import jakarta.persistence.Column;
@@ -9,8 +11,18 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserEntity {
     
     @Id
@@ -20,16 +32,34 @@ public class UserEntity {
     @Column(name = "username", nullable = false, unique=true)
     private String username;
 
-    @Column()
+    @Column(nullable=false)
     private String password;
+
+    @Column(nullable=false, unique = true)
     private String email;
+
+    @Temporal(TemporalType.DATE)
+    @Column(name="date_of_birth")
+    private Date dateBirth;
+
+    @Column(name="profile_picture")
+    private String profilePicture;
+    
     private String bio;
-    private List<Post> posts = new ArrayList<>();
 
-    private List<Group> groups = new ArrayList<>();
+    @Column(name="date_joined", nullable = false)
+    private LocalDateTime dateJoined;
 
-    @OneToMany(mappedBy = "author")
-    private  List<PostEntity> posts;
+    @Column(name="last_login")
+    private LocalDateTime  lastLogin;
+
+    private String status;
+
+    @OneToMany(mappedBy="autor")
+    private List<PostEntity> posts = new ArrayList<>();
+
+
+    private List<GroupEntity> groups = new ArrayList<>();
 
 
 }
